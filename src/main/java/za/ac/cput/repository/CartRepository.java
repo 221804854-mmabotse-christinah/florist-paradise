4c4170e365d046843ce17feda85ddd77d5df9408
mabotse21 <221804854@mycput.ac.za>
2024-08-12 12:43:48 +0200

    Cart, Favorite, product and review entity

--- src/main/java/za/ac/cput/repository/CartRepository.java
+++ src/main/java/za/ac/cput/repository/CartRepository.java
@@ -3,6 +3,7 @@ package za.ac.cput.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import za.ac.cput.domain.Cart;
+
 @Repository
 public interface CartRepository  extends JpaRepository<Cart, Long> {
     Cart findByCartId(Long id);
